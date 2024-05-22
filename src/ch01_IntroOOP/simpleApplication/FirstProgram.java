e9b9f48499f8e924ef74ef0d6a3ad7c63fd268fa
gemansipator <gemansipator@gmail.com>
2024-05-22 20:32:13 +0300

    simpleApplication

---
+++ src/ch01_IntroOOP/simpleApplication/FirstProgram.java
@@ -0,0 +1,14 @@
+package ch01_IntroOOP.simpleApplication;
+
+public class FirstProgram {
+    public static void main(String[] args) {
+
+        // объявление и создание объекта
+        TutorialAction action = new TutorialAction();
+
+        // вызов метода, который выводит строку
+        action.printMessage("tutorial-> https://docs.oracle.com/javase/tutorial/");
+
+
+    }
+}
---
+++ src/ch01_IntroOOP/simpleApplication/TutorialAction.java
@@ -0,0 +1,8 @@
+package ch01_IntroOOP.simpleApplication;
+
+public class TutorialAction {
+    void printMessage(String msg) { // method definition
+// output string
+        System.out.println(msg);
+    }
+}
