f988808effd2f55bede9e5eb819ae5e1c46c385a
gemansipator <gemansipator@gmail.com>
2024-05-30 21:53:05 +0300

    Задания к главе 1 Вариант A 6.Вывести фамилию разработчика, дату и время получения задания, а также   дату и время сдачи задания.

---
+++ src/ch01_IntroOOP/homeworkch01a/Option6.java
@@ -0,0 +1,26 @@
+package ch01_IntroOOP.homeworkch01a;
+
+/*Вывести фамилию разработчика, дату и время получения задания, а также
+дату и время сдачи задания.*/
+
+import java.util.Date;
+import java.util.Scanner;
+
+public class Option6 {
+    public static void main(String[] args) {
+        Date date1 = new Date();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите имя разработчика: ");
+        String name = scanner.nextLine();
+        System.out.println("Имя разработчика - " + name +" Получил задание "+ date1);
+        System.out.println("Разработчик выполнил задание? Если да, то введите слово да");
+        String result = scanner.nextLine();
+        if (result.equals("да")){
+            Date date2 = new Date();
+            System.out.println("Разработчик - " + name +" сдал задание "+ date2);
+        } else {
+            System.out.println("Вы ввели неверное значение");
+        }
+        System.out.println();
+    }
+}
