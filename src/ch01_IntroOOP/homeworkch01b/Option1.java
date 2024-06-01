6975922bf16788c7d233bb4842bc7f7beba7078b
gemansipator <gemansipator@gmail.com>
2024-06-01 16:38:31 +0300

    Задания к главе 1 Вариант В Ввести с консоли n целых чисел. На консоль вывести: 1. Четные и нечетные числа

---
+++ src/ch01_IntroOOP/homeworkch01b/Option1.java
@@ -0,0 +1,51 @@
+package ch01_IntroOOP.homeworkch01b;
+/*Вариант B
+Ввести с консоли n целых чисел. На консоль вывести:
+1. Четные и нечетные числа.*/
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Option1 {
+    public static void main(String[] args){
+
+        //Ввод в консоль n целых чисел:
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Сколько целых чисел вы собираетесь ввести? ");
+        int size = scanner.nextInt();
+        int[] sourceNumbers = new int[size];   //Создаем массив с заданным числом ячеек
+        System.out.print("Введите целые числа через пробел " + "и нажмите <Enter>: ");
+        for (int i = 0; i < sourceNumbers.length; i++) {
+            sourceNumbers[i] = scanner.nextInt();       //ЗАПОЛНЯЕМ МАССИВ ЗНАЧЕНИЯМИ
+        }
+        System.out.println("Ваш массив: " + Arrays.toString(sourceNumbers));
+
+
+        //x % 2 - вычисляет остаток от деления x на 2.
+        int counter;
+        int  evenNumbers;  //четные числа
+        int oddNumbers;   //нечетные числа
+
+        System.out.println("Четные числа: ");
+        for (counter = 0; counter < sourceNumbers.length; counter++){
+            int namber = sourceNumbers[counter];
+            if(namber%2 == 0){
+                evenNumbers = namber;
+
+                System.out.print(" "+evenNumbers);
+            }
+        }
+        System.out.println();
+        System.out.println("Нечетные числа: ");
+        for (counter = 0; counter < sourceNumbers.length; counter++){
+            int namber = sourceNumbers[counter];
+            if(namber%2 != 0){
+                oddNumbers = namber;
+                System.out.print(" "+oddNumbers);
+            }
+        }
+
+//        while (List = scanner.nextLine())
+
+    }
+}
